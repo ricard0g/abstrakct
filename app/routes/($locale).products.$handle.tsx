bd9891401620b5e5cba36646ba3d9ffafe212f67
ricard0g <ricardoguzman444@gmail.com>
2025-05-03 13:42:42 +0200

    feat(product page):
    - Fetch data from metafields for each product
    - Create modularity in UI components for Product Page

--- app/routes/($locale).products.$handle.tsx
+++ app/routes/($locale).products.$handle.tsx
@@ -13,6 +13,36 @@ import {ProductImage} from '~/components/ProductImage';
 import {ProductForm} from '~/components/ProductForm';
 import {animated, useSpring} from '@react-spring/web';
 import {AddToCartButton} from '~/components/AddToCartButton';
+import type {ProductVariant} from '@shopify/hydrogen/storefront-api-types';
+
+type Metafield = {
+  id: string;
+  key: string;
+  value: string;
+};
+
+type ProductCopy = {
+  'description-section': {
+    'concise-heading': string;
+    'concise-description': string;
+  };
+  'animated-heading': string;
+  'history-section': {
+    heading: string;
+    'first-block': {
+      'text-block': string;
+      image_url: string;
+    };
+    'second-block': {
+      'text-block': string;
+      image_url: string;
+    };
+    'third-block': {
+      'text-block': string;
+      image_url: string;
+    };
+  };
+};
 
 export const meta: MetaFunction<typeof loader> = ({data}) => {
   return [
@@ -52,7 +82,36 @@ async function loadCriticalData({
 
   const [{product}] = await Promise.all([
     storefront.query(PRODUCT_QUERY, {
-      variables: {handle, selectedOptions: getSelectedProductOptions(request)},
+      variables: {
+        handle,
+        selectedOptions: getSelectedProductOptions(request),
+        metafieldIdentifiers: [
+          {
+            namespace: 'custom',
+            key: 'product_copy',
+          },
+          {
+            namespace: 'custom',
+            key: 'painting_location',
+          },
+          {
+            namespace: 'custom',
+            key: 'style',
+          },
+          {
+            namespace: 'custom',
+            key: 'medium',
+          },
+          {
+            namespace: 'custom',
+            key: 'year',
+          },
+          {
+            namespace: 'custom',
+            key: 'artist',
+          },
+        ],
+      },
     }),
     // Add other queries here, so that they are loaded in parallel
   ]);
@@ -108,46 +167,13 @@ export default function Product() {
     selectedOrFirstAvailableVariant: selectedVariant,
   });
 
-  const {title, descriptionHtml} = product;
+  const {title, metafields} = product;
 
   return (
     <div className="h-full z-0">
       <ProductImage image={selectedVariant?.image} />
-      <section className="flex flex-col items-center justify-center w-full max-h-full h-[90vh] mx-auto overflow-hidden -z-20">
-        <div className="flex flex-col items-start justify-center relative w-full h-full overflow-hidden">
-          <h1 className="inline-flex whitespace-nowrap mb-5 animate-carousel">
-            {/* First set of titles */}
-            <span className="block overflow-hidden">
-              {[...Array(2)].map((_, i) => (
-                <animated.span
-                  style={textSprings}
-                  key={i}
-                  className="inline-block text-[200px] text-transparent bg-clip-text bg-gradient-to-br from-stone-900 via-zinc-500 to-gray-800 bg-[size:200%_200%] font-display font-normal mx-5 animate-bg-rotate -z-20"
-                >
-                  {title}
-                </animated.span>
-              ))}
-            </span>
-            {/* Duplicate set to create seamless loop */}
-            <span className="block overflow-hidden">
-              {[...Array(2)].map((_, i) => (
-                <animated.span
-                  style={textSprings}
-                  key={`dup-${i}`}
-                  className="inline-block text-[200px] text-transparent bg-clip-text bg-gradient-to-br from-slate-900 via-gray-500 to-neutral-800 bg-[size:200%_200%] font-display font-normal mx-5 animate-bg-rotate -z-20"
-                >
-                  {title}
-                </animated.span>
-              ))}
-            </span>
-          </h1>
-          <div className="block overflow-hidden">
-            <animated.h2 style={textSprings} className="my-0">
-              <Money className="text-6xl" data={selectedVariant.price} />
-            </animated.h2>
-          </div>
-        </div>
-      </section>
+      <ProductHero title={title} selectedVariant={selectedVariant} />
+      <ProductDescription metafields={metafields} />
       <Analytics.ProductView
         data={{
           products: [
@@ -163,6 +189,98 @@ export default function Product() {
           ],
         }}
       />
+    </div>
+  );
+}
+
+function ProductHero({
+  title,
+  selectedVariant,
+}: {
+  title: string;
+  selectedVariant: ProductVariant;
+}) {
+  const [textSprings] = useSpring(
+    () => ({
+      from: {
+        y: '100%',
+      },
+      to: {
+        y: '0',
+      },
+    }),
+    [],
+  );
+
+  return (
+    <section className="flex flex-col items-center justify-center w-full max-h-full h-[90vh] mx-auto overflow-hidden -z-20">
+      <div className="flex flex-col items-start justify-center relative w-full h-full overflow-hidden">
+        <h1 className="inline-flex whitespace-nowrap mb-5 animate-carousel">
+          {/* First set of titles */}
+          <span className="block overflow-hidden">
+            {[...Array(1)].map((_, i) => (
+              <animated.span
+                style={textSprings}
+                key={i}
+                className="inline-block text-[200px] text-transparent bg-clip-text bg-gradient-to-br from-stone-900 via-zinc-500 to-gray-800 bg-[size:200%_200%] font-display font-normal mx-5 animate-bg-rotate -z-20"
+              >
+                {title}
+              </animated.span>
+            ))}
+          </span>
+          {/* Duplicate set to create seamless loop */}
+          <span className="block overflow-hidden">
+            {[...Array(1)].map((_, i) => (
+              <animated.span
+                style={textSprings}
+                key={`dup-${i}`}
+                className="inline-block text-[200px] text-transparent bg-clip-text bg-gradient-to-br from-slate-900 via-gray-500 to-neutral-800 bg-[size:200%_200%] font-display font-normal mx-5 animate-bg-rotate -z-20"
+              >
+                {title}
+              </animated.span>
+            ))}
+          </span>
+        </h1>
+        <div className="block overflow-hidden">
+          <animated.h2 style={textSprings} className="my-0">
+            <Money className="text-6xl" data={selectedVariant.price} />
+          </animated.h2>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function ProductDescription({metafields}: {metafields: Metafield[]}) {
+  const productCopyMetafield = metafields.find(
+    (metafield: Metafield) => metafield.key === 'product_copy',
+  );
+
+  const productCopy = productCopyMetafield?.value
+    ? (JSON.parse(productCopyMetafield.value) as ProductCopy)
+    : null;
+
+  const paintingLocation: string | undefined = metafields.find(
+    (metafield: Metafield) => metafield.key === 'painting_location',
+  )?.value;
+  const style: string | undefined = metafields.find(
+    (metafield: Metafield) => metafield.key === 'style',
+  )?.value;
+  const medium: string | undefined = metafields.find(
+    (metafield: Metafield) => metafield.key === 'medium',
+  )?.value;
+  const year: string | undefined = metafields.find(
+    (metafield: Metafield) => metafield.key === 'year',
+  )?.value;
+  const artist: string | undefined = metafields.find(
+    (metafield: Metafield) => metafield.key === 'artist',
+  )?.value;
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h2 className="text-4xl font-bold">
+        {productCopy?.['description-section']['concise-heading']}
+      </h2>
     </div>
   );
 }
@@ -237,6 +355,11 @@ const PRODUCT_FRAGMENT = `#graphql
     adjacentVariants (selectedOptions: $selectedOptions) {
       ...ProductVariant
     }
+    metafields(identifiers: $metafieldIdentifiers) {
+      id
+      key
+      value
+    }
     seo {
       description
       title
@@ -251,6 +374,7 @@ const PRODUCT_QUERY = `#graphql
     $handle: String!
     $language: LanguageCode
     $selectedOptions: [SelectedOptionInput!]!
+    $metafieldIdentifiers: [HasMetafieldsIdentifier!]!
   ) @inContext(country: $country, language: $language) {
     product(handle: $handle) {
       ...Product
